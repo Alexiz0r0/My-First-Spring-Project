400607a6b1fa75d06c6774231ce34f4d95f2ddb0
Alexiz0r0 <jaob.4.95@gmail.com>
2023-01-19 07:12:04 -0500

    security jwt jan19

---
+++ auth-service/src/main/java/com/project/authservice/entity/AuthUser.java
@@ -0,0 +1,22 @@
+package com.project.authservice.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
+@Entity
+public class AuthUser {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String userName;
+    private String password;
+
+}
