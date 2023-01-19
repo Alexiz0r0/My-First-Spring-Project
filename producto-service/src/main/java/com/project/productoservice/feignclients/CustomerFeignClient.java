480370cb6f44936498f5177f7b32d56a00c437c6
Alexiz0r0 <jaob.4.95@gmail.com>
2023-01-19 03:04:50 -0500

    gateway v3

--- producto-service/src/main/java/com/project/productoservice/feignclients/CustomerFeignClient.java
+++ producto-service/src/main/java/com/project/productoservice/feignclients/CustomerFeignClient.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @FeignClient(name = "cliente-service")
+@RequestMapping("/customerApi")
 public interface CustomerFeignClient {
 
     /*
--- producto-service/src/main/java/com/project/productoservice/feignclients/SupplierFeignClient.java
+++ producto-service/src/main/java/com/project/productoservice/feignclients/SupplierFeignClient.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @FeignClient(name = "proveedor-service")
+@RequestMapping("/supplierApi")
 public interface SupplierFeignClient {
 
     /*
